0905178742b8fbfd938046fdd6697ff2902e1bb6
ssghost <ssprof0@gmail.com>
2023-02-25 09:17:07 +0800

    update config

--- my-app/src/config.ts
+++ my-app/src/config.ts
@@ -10,7 +10,7 @@ export const DEFAULT_CONTRACT_INDEX = BigInt(81);
 export const MAX_CONTRACT_EXECUTION_ENERGY = BigInt(30000);
 export const PING_INTERVAL_MS = 5000;
 
-const TESTNET_GENESIS_BLOCK_HASH = '4221332d34e1694168c2a0c0b3fd0f273809612cb13d000d5c2e00e85f50f796';
+const TESTNET_GENESIS_BLOCK_HASH = '';
 export const TESTNET: typeof Network = {
     name: 'testnet',
     genesisHash: TESTNET_GENESIS_BLOCK_HASH,
@@ -18,12 +18,12 @@ export const TESTNET: typeof Network = {
     ccdScanBaseUrl: 'https://testnet.ccdscan.io',
 };
 
-const WALLET_CONNECT_PROJECT_ID = '76324905a70fe5c388bab46d3e0564dc';
+const WALLET_CONNECT_PROJECT_ID = '';
 const WALLET_CONNECT_OPTS: typeof SignClientTypes.Options = {
     projectId: WALLET_CONNECT_PROJECT_ID,
     metadata: {
-        name: 'myStorage',
-        description: 'Example dApp',
+        name: 'ModelWeightsStorage',
+        description: 'Hackathon dApp',
         url: '#',
         icons: ['https://walletconnect.com/walletconnect-logo.png'],
     },
